7c1031ad5fb422fd734614dcc4c934a383e59b3d
Mrowqa <artur.jamro@gmail.com>
2017-06-02 22:40:13 +0200

    Created Socket class

---
+++ common/UdpSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mrowqa on 2017-06-02.
+//
+
+#include "UdpSocket.hpp"
---
+++ common/UdpSocket.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Mrowqa on 2017-06-02.
+//
+
+#ifndef SIKTACKA_UDPSOCKET_HPP
+#define SIKTACKA_UDPSOCKET_HPP
+
+
+class UdpSocket {
+
+};
+
+
+#endif //SIKTACKA_UDPSOCKET_HPP
